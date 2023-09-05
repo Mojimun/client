def5d8d87451fd3dd66820e80d58f165c5468215
Mojimun Islam <mojimunislam@gmail.com>
2023-09-05 21:18:22 +0530

    frontend

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import "./header.css";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <div className="headerTitles">
+        <span className="headerTitleSm">FOOD BLOG</span>
+      </div>
+      <img
+        className="headerImg"
+        src="https://wallpapersdsc.net/wp-content/uploads/2016/09/Junk-Food-Pictures.jpg"
+        alt=""
+      />
+    </div>
+  );
+}
